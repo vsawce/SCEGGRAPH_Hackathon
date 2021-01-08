25c06682e6ad82c7829d17719db005bb7ceede86
vsawce <vincent.saw@sjsu.edu>
2021-01-08 15:37:25 -0800

    Initial commit of AVR Atmel project

---
+++ avr/Errand_Display/Errand_Display/main.c
@@ -0,0 +1,18 @@
+/*
+ * Errand_Display.c
+ *
+ * Created: 1/8/2021 3:36:49 PM
+ * Author : vsaw3
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
