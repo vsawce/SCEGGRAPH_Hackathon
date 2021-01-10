4eb03000ca96d774ec61351703adb9524fa56c72
vsawce <vincent.saw@sjsu.edu>
2021-01-10 09:17:29 -0800

    Included buttons driver. Utilizes polling as interrupts caused incompatibility with the 5110 LCD

---
+++ avr/Errand_Display/Errand_Display/buttons.c
@@ -0,0 +1,21 @@
+﻿#include "buttons.h"
+
+
+void buttons__poll() {
+	uint16_t timer = 0;
+	while (!(PINC & (1<<BUT1))) {
+		timer++;
+		_delay_ms(1);
+	}
+	if (timer > DEBOUNCE_TIME) {
+		nokia__select_state();
+	}
+	
+	timer = 0;
+	while (!(PINC & (1<<BUT2))) {
+		timer++;
+		_delay_ms(1);
+	}
+	if (timer > DEBOUNCE_TIME)
+		nokia__next_state();
+}
